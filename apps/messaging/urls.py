5ba8582f15b0fddcd6dcb58c7aabf55b5c912481
Praise Idowu <ifeoluwapraise02@gmail.com>
2025-11-28 23:42:16 +0100

    Remove caching from message detail view in URL patterns

--- apps/messaging/urls.py
+++ apps/messaging/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
+# from django.views.decorators.cache import cache_page
 from .import views
 
 app_name = 'messages'
 
 urlpatterns = [
     path('inbox/', views.Inbox.as_view(), name='inbox'),
-    path('message/<str:id>/', cache_page(60 * 15)(views.ViewMessage.as_view()), name='message'),
+    # path('message/<str:id>/', cache_page(60 * 15)(views.ViewMessage.as_view()), name='message'),
+    path('message/<str:id>/', views.ViewMessage.as_view(), name='message'),
     path('message/create-message/<str:id>/', views.CreateMessage.as_view(), name='create_message'),
     path('message/delete-message/<str:id>/', views.DeleteMessage.as_view(), name='delete_message'),
 ]
